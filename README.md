179a429892f31f4e3e122a92348ee20a8aea497f
Sean Doherty <sean.doherty@crosscomm.net>
2018-02-21 22:16:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Sunglasses.io Server Starter
+
+This project is a starter for the Sunglasses.io eval.  
+
+Don't forget to update this file with helpful hints on how to run the application.
---
+++ app/server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+var finalHandler = require('finalhandler');
+var queryString = require('querystring');
+var Router = require('router');
+var bodyParser   = require('body-parser');
+var uid = require('rand-token').uid;
+
+const PORT = 3001;
+
+http.createServer(function (request, response) {
+
+}).listen(PORT);
