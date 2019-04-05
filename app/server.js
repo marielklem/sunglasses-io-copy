3a12a257f622dd11c12dcbcb818bf9fe0c629911
marielklem <mariel.steinbeiser@gmail.com>
2019-04-05 16:32:08 -0400

    Tests for /brands/{id}/products and code working

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var finalHandler = require('finalhandler');
 var queryString = require('querystring');
 var Router = require('router');
-var bodyParser   = require('body-parser');
+var bodyParser = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
@@ -12,10 +12,15 @@ let brands = [];
 let products = [];
 let users = [];
 
+// Setup state
+
+
+
 //Setup router
 var myRouter = Router();
 myRouter.use(bodyParser.json());
 
+//Create a server
 //Create a server
 const server = http.createServer(function (request, response) {
   myRouter(request, response, finalHandler(request, response))
@@ -23,15 +28,36 @@ const server = http.createServer(function (request, response) {
   if(error) {
     throw error
   }
-  fs.readFile("initial-data/brands.json", "utf8", (err, data) => {
-    brands = JSON.parse(data)
-  });
+  brands = JSON.parse(fs.readFileSync("initial-data/brands.json", "utf8"))
+
+  products = JSON.parse(fs.readFileSync("initial-data/products.json", "utf8"))
 });
 
 //Get all the brands
-myRouter.get('/api/brands', function(request, response) {
-  response.writeHead(200, Object.assign({'Content-Type': 'application/json'}))
+myRouter.get('/api/brands', function (request, response) {
+  response.writeHead(200, Object.assign({ 'Content-Type': 'application/json' }))
   response.end(JSON.stringify(brands))
+})
+
+myRouter.get('/api/brands/:id/products', function (request, response) {
+  //verify that a brand exists with that ID
+  let brandId = brands.find((brand) => {
+    return brand.id == request.params.id
+  })
+  if (!brandId) {
+    //if there is no brand with that ID, return a 404
+    response.writeHead(404, "That brand cannot be found");
+    response.end();
+    return;
+  } else {
+    const brandProduct =  products.filter(product => {
+      if (brandId.id == product.brandId) {
+        return product
+      }
+    })
+    response.writeHead(200, Object.assign({ 'Content-Type': 'application/json' }))
+    response.end(JSON.stringify(brandProduct))
+  }
 })
 
 
--- test/sunglasses-test.js
+++ test/sunglasses-test.js
@@ -2,7 +2,7 @@ let chai = require('chai')
 let { expect } = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../app/server');
-
+let should = chai.should();
 chai.use(chaiHttp);
 
 describe('The API', () => {
@@ -19,7 +19,33 @@ describe('The API', () => {
           expect(response.body).to.have.lengthOf(5);
           done();
         });
+    });
+  });
+
+  describe('/GET api/brands/:id/products', () => {
+    it('it should send an error if no id was selected', done => {
+      chai
+        .request(server)
+        .get('/api/brands/99/products')
+        // assert
+        .end((error, response) => {
+          expect(response).to.have.status(404);
+          done();
+        });
     })
   })
+  it('it should GET all the products for a specific brand', done => {
+    //act
+    chai
+      .request(server)
+      .get('/api/brands/2/products')
+      // assert
+      .end((error, response) => {
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.lengthOf(2);
+        done();
+      });
+  })
 })
+
 
