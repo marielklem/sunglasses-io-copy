18fcb8f2711b0ad7d5c8721fa7f9bb965e70a669
marielklem <mariel.steinbeiser@gmail.com>
2019-04-05 09:59:09 -0400

    Updated Swagger file with basic layout and beginning of server

--- app/server.js
+++ app/server.js
@@ -7,7 +7,28 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
+//set holding variables
+var brands = [],
+var products = [],
+var users = []
 
+//Setup router
+var myRouter = Router();
+myRouter.use(bodyParser.json());
+
+//Create a server
 http.createServer(function (request, response) {
+  myRouter(request, response, finalHandler(request, response))
+}).listen(PORT, () => {
+  fs.readFile("/initial-data/brands.json", "utf8", (err, data) => {
+    brands = JSON.parse(data)
+  });
 
-}).listen(PORT);
+  fs.readFile("/initial-data/products", "utf8", (err, data) => {
+    products = JSON.parse(data)
+  });
+
+  fs.readFile("/initial-data/users", "utf8", (err, data) => {
+    users = JSON.parse(data)
+  })
+});
